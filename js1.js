d12d00fbf79c0172685bbc0a252bb7190c5a75f7
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2023-03-03 00:38:11 +0600

    added featur and name

--- js1.js
+++ js1.js
@@ -20,8 +20,23 @@ const showFetch = data => {
                 <p class="m-0">1. ${singleItem.features[0]}</p>
                 <p class="m-0">2. ${singleItem.features[1]}</p>
                 <p class="m-0">3. ${singleItem.features[2]}</p>
+                
              </div>
              <span class="border mx-3 border-top-0 border-light-subtle"></span>
+             <div class="mx-3 mt-4">
+                 <div class="d-flex justify-content-between align-items-center gap-2">
+                    <div>
+                        <h4>${singleItem.name}</h4>
+                        <div class="d-flex align-itemx-center gap-2">
+                         <i class="fa-regular fa-calendar mt-1 text-black"></i>
+                         <p class="">${singleItem.published_in}</p>
+                        </div>
+                    </div>
+                     <div>
+                         <i class="fa-solid fa-arrow-right"></i>
+                     </div>
+                 </div>
+             </div>
         </div>
         `;
         parantDiv.appendChild(childDiv);
